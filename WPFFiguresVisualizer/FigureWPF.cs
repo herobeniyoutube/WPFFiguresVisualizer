85f714730388e8a33fb8b02250f381f0ee3a72f6
herobeniyoutube <arseny.barinov@yahoo.com>
2024-03-17 22:51:43 +0300

    Добавьте файлы проекта.

---
+++ WPFFiguresVisualizer/FigureWPF.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using Rectangle = System.Windows.Shapes.Rectangle;
+
+namespace WPFFiguresVisualizer
+{
+    public class FigureWPF
+    {
+        public static Shape Get(string Shape, double hight, double width)
+        {
+            Shape figure = null;
+            double side = 0;
+            switch (Shape)
+            {
+                case "Square":
+                    side = width;
+                    figure = new Rectangle();
+                    figure.Width = side;
+                    figure.Height = side;
+                    return figure;
+                case "Ellipse":
+                    figure = new Ellipse();
+                    figure.Width = width;
+                    figure.Height = hight;
+                    return figure;
+                case "Rectangle":
+                    figure = new Rectangle();
+                    figure.Width = width;
+                    figure.Height = hight;
+                    return figure;
+                case "Circle":
+                    side = width;
+                    figure = new Ellipse();
+                    figure.Width = side;
+                    figure.Height = side;
+                    return figure;
+
+            }
+            return figure;
+        }
+        public static Shape Get(PointCollection points)
+        {
+            Polygon figure = new Polygon();
+            figure.Points = points;
+            return figure;
+        }
+    }
+}
---
+++ WPFFiguresVisualizer/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFFiguresVisualizer
+{
+    public partial class MainWindow : Window
+    {
+        private static PointCollection points = new PointCollection();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void ShowFigure(object sender, RoutedEventArgs e)
+        {
+            FigurePlaceholder.Children.Clear();
+            Button button = (Button)e.Source;
+            string buttonName = button.Name;
+            Shape shape;
+
+            if (buttonName == "Triangel")
+            {
+                points.Add(new Point(100, 0));
+                points.Add(new Point(0, 100));
+                points.Add(new Point(200, 100));
+                shape = FigureWPF.Get(points);
+            }
+            else
+            {
+                double heigth = 200;
+                double width = 100;
+                shape = FigureWPF.Get(buttonName, heigth, width);
+
+            }
+
+            shape.Fill = new SolidColorBrush(Colors.Blue);
+            FigurePlaceholder.Children.Add(shape);
+            Canvas.SetTop(FigurePlaceholder.Children[0], 117);
+            Canvas.SetLeft(FigurePlaceholder.Children[0], 300);
+        }
+        private void HideCircle(object sender, RoutedEventArgs e)
+        {
+            FigurePlaceholder.Children.Clear();
+        }
+    }
+}
